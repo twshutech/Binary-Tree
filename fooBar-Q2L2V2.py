c875029d7fc6bd000d4a2f7c1f08d33ad1e0814e
shuwei <38966382+twshutech@users.noreply.github.com>
2020-09-03 08:07:27 +0800

    Add passed version.

--- fooBar-Q2L2V2.py
+++ fooBar-Q2L2V2.py
@@ -1,19 +1,41 @@
-subtrahend = []
-minuend = []
-history = []
-cycle = []
+def countLooping(history):
+  cycle = []
+  c = 0
+  for item in history:
+    c = history.count(item)
+    if c > 1 and item not in cycle:
+      cycle.append(item)
+  return len(cycle)
+
+def borrow(minuend, r, s, base):
+  if r - s >= 0:
+    if minuend[r - s] - 1 < 0:
+      if s<len(minuend) and r>s:
+        minuend[r - s] = base - 1
+        s += 1
+        return borrow(minuend, r, s, base)
+    else:
+      minuend[r - s] -= 1
+      return minuend
 
 def solution(n, base):
+  global subtrahend, minuend, history, cycle, subtraction, sortList
+  subtrahend = minuend = history = cycle = []
+
+  subtraction = lambda minuend, subtrahend: minuend-subtrahend
+  sortList = lambda arr, order: sorted(arr, reverse=order)
+  return(prepareVariable(n, base))
+
+def prepareVariable(n, base):
   k = len(n)
   arr = []
   for q in n:
     arr.append(q)
-  arr.sort() # ascending
-  subtrahend = ''.join(arr)
-  arr.sort(reverse=True) # descending
-  minuend = ''.join(arr)
-
+  subtrahend = ''.join(sortList(arr, False))# ascending
+  minuend = ''.join(sortList(arr, True))# descending
   Subtractor(minuend, subtrahend, base, k)
+  if cycleChecking() == True:
+    return countLooping(history)
 
 def itemToInt(list):
   newArr = []
@@ -26,39 +48,30 @@ def cycleChecking():
   isLooping = False
   for item in history:
     c = history.count(item)
-    if c > 5:
+    if c > 2:
       isLooping = True
-  #     cycle.append(c)
-  # if len(cycle) != 0:
-  #   for cycleItem in cycle:
-  #     cycleIndex.append(history.index(cycleItem))
-  # print(cycleIndex)
   return isLooping
 
 def Subtractor(minuend, subtrahend, base, k):
   minuend = itemToInt(minuend)
   subtrahend = itemToInt(subtrahend)
-  result = []
+  rawResult = []
   for i in range(k):
     d = 0
     r = k - i - 1
 
-    if minuend[r] > subtrahend[r]:
+    if minuend[r] >= subtrahend[r]:
       d = minuend[r] - subtrahend[r]
     elif minuend[r] < subtrahend[r]:
       d = base - subtrahend[r] + minuend[r]
-      if r - 1 != -1:
-        minuend[r - 1] = minuend[r - 1]
-        minuend[r - 1] = minuend[r - 1] - 1
+      minuend[r] = d
+      #minuend[r - 1] = base - 1
+      minuend = borrow(minuend, r, 1, base)
 
-    result.append(d)
-  result.sort(reverse=False)
+    rawResult.append(d)
+  rawResult.reverse()
 
-  newN = ''.join([str(elem) for elem in result]) 
+  newN = ''.join([str(elem) for elem in rawResult]) 
   history.append(newN)
   if cycleChecking() == False:
-    solution(newN, 3)
-  else:
-    print(history)
-#solution('1211', 10)
-solution('210022', 3)
+    prepareVariable(newN, base)
