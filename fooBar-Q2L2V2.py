0eda70168b0a7a4e719cd69c1329262ba5bdf42b
Josh Kuo <Josh.Kuo@dlinkcorp.com>
2020-10-25 10:39:04 +0800

    q2l2-v1.2.0

--- fooBar-Q2L2V2.py
+++ fooBar-Q2L2V2.py
@@ -3,22 +3,28 @@ minuend = []
 history = []
 cycle = []
 
+greater = lambda a,b: a>b
+subtraction = lambda a, b: a - b
+borrowedSubtraction = lambda a, b, c:  c - b + a
+sortList = lambda a, b : sorted(a, reverse=b)
+appendInt = lambda a, b : b.append(int(a))
+appendStr = lambda a, b : b.append(str(a))
+
 def solution(n, base):
   k = len(n)
   arr = []
   for q in n:
-    arr.append(q)
-  arr.sort() # ascending
-  subtrahend = ''.join(arr)
-  arr.sort(reverse=True) # descending
-  minuend = ''.join(arr)
+    appendStr(q, arr)
+
+  subtrahend = ''.join(sortList(arr, False))# ascending
+  minuend = ''.join(sortList(arr, True))# descending
 
   Subtractor(minuend, subtrahend, base, k)
 
-def itemToInt(list):
+def itemToInt(listN):
   newArr = []
-  for item in list:
-    newArr.append(int(item))
+  for item in listN:
+    appendInt(item, newArr)
   return newArr
 
 def cycleChecking():
@@ -26,14 +32,21 @@ def cycleChecking():
   isLooping = False
   for item in history:
     c = history.count(item)
-    if c > 5:
+    if c > 10:
       isLooping = True
-  #     cycle.append(c)
-  # if len(cycle) != 0:
-  #   for cycleItem in cycle:
-  #     cycleIndex.append(history.index(cycleItem))
-  # print(cycleIndex)
+      if int(item) not in cycle:
+        appendInt(item, cycle)
+      #if len(cycle) != 0:
+      #  print(cycle)
   return isLooping
+
+def borrow(i, r, minuend, s):
+  if minuend[r - s] < 1:
+    minuend -= 1
+    return minuend
+  else:
+    s += 1
+    borrow(i, r, minuend, s)
 
 def Subtractor(minuend, subtrahend, base, k):
   minuend = itemToInt(minuend)
@@ -43,16 +56,16 @@ def Subtractor(minuend, subtrahend, base, k):
     d = 0
     r = k - i - 1
 
-    if minuend[r] > subtrahend[r]:
-      d = minuend[r] - subtrahend[r]
-    elif minuend[r] < subtrahend[r]:
-      d = base - subtrahend[r] + minuend[r]
-      if r - 1 != -1:
-        minuend[r - 1] = minuend[r - 1]
+    print(r,i)
+    if greater(minuend[r], subtrahend[r]): 
+      d = subtraction(minuend[r], subtrahend[r])
+    elif greater(subtrahend[r], minuend[r]):
+      d = borrowedSubtraction(minuend[r], subtrahend[r], base)
+      if r > 1:
         minuend[r - 1] = minuend[r - 1] - 1
 
-    result.append(d)
-  result.sort(reverse=False)
+    appendInt(d, result)
+  result = sortList(result, False)
 
   newN = ''.join([str(elem) for elem in result]) 
   history.append(newN)
@@ -60,5 +73,6 @@ def Subtractor(minuend, subtrahend, base, k):
     solution(newN, 3)
   else:
     print(history)
+
 #solution('1211', 10)
 solution('210022', 3)
